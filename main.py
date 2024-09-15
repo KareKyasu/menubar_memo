d25c0f3e7eb7846fc7014f3cb7d1555272aa7e8a
KareKyasu <>
2024-09-15 18:35:47 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import rumps
+
+# デバッグモードを有効化
+# rumps.debug_mode(True)
+
+
+# メニューアイテムをクリックしたときの動作
+class MemoApp(rumps.App):
+    def __init__(self):
+        super(MemoApp, self).__init__("Memo App")
+        self.menu = ["Title"]
+
+    @rumps.clicked("Title")
+    def c(self, _):
+        # テキスト入力用のウィンドウを作成
+        window = rumps.Window(
+            title="Enter memo",
+            message="Please enter memo:",
+            ok="OK",
+            cancel="Cancel",
+            dimensions=(200, 24),
+        )  # テキスト入力フィールドのサイズを指定
+
+        # ウィンドウを表示してユーザーの入力を取得
+        response = window.run()
+
+        # ユーザーの入力をチェックして表示
+        if response.clicked:
+            # print("User entered:", response.text)
+            self.title = response.text
+        else:
+            print("User canceled the input.")
+
+
+if __name__ == "__main__":
+
+    MemoApp().run()
